7a9c943181cc8bc252f5944e42630b2c6ef3bcba
Will St. Onge <ongewill@gmail.com>
2020-11-16 17:47:11 -0500

    Ganga added plotting code

--- final.py
+++ final.py
@@ -1,4 +1,5 @@
 import matplotlib.axes, matplotlib.figure, matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 import numpy as np, requests, io, pandas as pd
 
 def main():
@@ -13,45 +14,45 @@ def main():
     figure3(data)
     figure4(data)
     figure5(data)
-            
+    print(data)
+
 
 def figure1(data):
-    test = None # placeholder
+    plt.plot(data[:, 1],'o', color='blue')
+    plt.title("Dividends vs Earnings")
+    plt.show()
+    #Need to make the X-Axis display the year
 
 
 def figure2(data):
-    test = None # placeholder
+    plt.plot(data[:, 2],'o', color='blue')
+    plt.plot(data[:, 3],'o', color='red')
+    plt.xlabel("Year")
+    plt.ylabel("S & P 500")
+    plt.title("S & P 500")
+    plt.show()
 
 
 def figure3(data):
-    test = None # placeholder
+    plt.plot(data[:, 2],'o', color='blue')
+    plt.plot(data[:, 7],'o', color='red')
+    plt.title("Dividends vs Real Dividends")
+    plt.show()
 
 
 def figure4(data):
-    test = None # placeholder
+    plt.plot(data[:, 3],'o', color='blue')
+    plt.plot(data[:, 8],'o', color='red')
+    plt.title("Earnings vs Real Earnings")
+    plt.show()
 
 
 def figure5(data):
-    test = None # placeholder
+    plt.plot((data[:, 3])/(data[:, 7]),'o', color='pink')
+    plt.title("Earnings per SP500")
+    plt.show()
 
 
 if __name__ == "__main__":
     main()
-
-"""
-
-ax: matplotlib.axes.Axes
-x = np.zeros((5, 5))
-y = np.zeros(5)
-
-fig, ax = plt.subplots()
-ax.scatter(x, y)
-
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_title("X vs Y")
-
-plt.save_fig("test.png")
-plt.show()
-
-"""
+    
